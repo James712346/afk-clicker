f47e661bf9b15c6a9357735f8a730b6a221cc783
James Prince <james712346@gmail.com>
2020-12-31 11:52:21 +1000

    Update script.sh
    Improve the script, adding an exit method and it will continue indefinitely, I also added a way to set the timeout length

--- linux/script.sh
+++ linux/script.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
-title=$1  # Window title or a portion of it, Mozilla Firefox can be firefox
-key=$2    # Key to send, 1 = left mouse, 2 = middle, 3 = right
-          # If you want a keystroke thant change "click" to "key" in the last command
-          # and pass the key you want to (it can be a combination).
+#!bin/sh
 
-win=$(xdotool search --onlyvisible -name $title | head -1)
+exitvar="" #Makes sure that the exitvar is empty
 
-xdotool click --clearmodifiers --repeat 100000000000000000 --delay 1000 --window $win $key
+#Sets up flags with getopts, this includes -w windowtitle -k keyid {1|left, 2|middle, 3|right} and -t timeout
+while getopts w:k:t: flag
+do
+    case "${flag}" in
+        w) windowtitle=${OPTARG};;
+        k) key=${OPTARG};;
+        t) timeout=${OPTARG};;
+    esac
+done
+
+win=$(xdotool search --onlyvisible -name $windowtitle | head -1) #get the windowid from the given -w argument
+
+echo "Starting AutoClicker on windowid $win clicking mouse $key with a $timeout seconds delay" # explains what its doing
+xdotool type "press enter to stop the script: " # Inputs some intrustions into the termal that is backlogs to the read command
+while [ -z "$exitvar" ] # checks if the exitvar is empty
+do
+  read -t $timeout exitvar # reads the users input with a timeout
+  xdotool click --window $win $key # sends click to the window specified in the users argument
+done
+echo "I have stop clicking ;)" # Prints a goodbye message
